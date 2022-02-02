f9fcdbe3a860821e00980750428a165788949261
Jay Nguyen <jay@Jays-iMac.local>
2022-02-02 15:45:20 -0800

    Updated Pending Project Description/Tools

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ function alertProject2() {
 function alertPending1() {
 	console.log('Pending Project 1 Alert Engaged');
 	alert(
-		'Project Name: Wizarding Schools Review Page\nProject Status: Pending\nProject Details: Sign up as a school Headmaster to upload school information and respond to reviews, or sign up as a student to leave a school review.\nTools: Node, React, JSX, & Express'
+		'Project Name: Wizarding Schools Review Page\nProject Status: Pending\nProject Details: Sign up as a school Headmaster to upload school information and respond to reviews, or sign up as a student to leave a school review.\nTools: Node, React, JSX, & Express, etc (more languages as project progresses).'
 	);
 }
